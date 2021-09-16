1a8f3a4005751b663fceef24810588cd9f7c3afe
void <90846203+InfinityOfVoid@users.noreply.github.com>
2021-09-16 18:24:57 +0300

    Update lesson2.7.2.c

--- lesson2.7/lesson2.7.2.c
+++ lesson2.7/lesson2.7.2.c
@@ -1,5 +1,12 @@
 #include "lesson2.7.1.c"
 
+/*
+struct list {
+    int64_t value;
+    struct list* next;
+};
+*/
+
 /* Изменить каждый элемент списка с помощью функции f  */
 void list_map_mut(struct list* l, int64_t (f) (int64_t))  {
     while(l != NULL) {
@@ -8,12 +15,9 @@ void list_map_mut(struct list* l, int64_t (f) (int64_t))  {
     }
 }
 
-
 static int64_t triple( int64_t x ) { return x * 3; }
-
-int64_t mult_int64_t(int64_t a) {return a*3;}
 
 /* Используя list_map_mut умножьте все элементы списка на 3 */
 void list_triple(struct list* l ) { 
-    list_map_mut(l, &mult_int64_t);
+    list_map_mut(l, &triple);
 }
