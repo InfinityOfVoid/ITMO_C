96a78917aa08e3d4c5947852c0736e818904629f
void <90846203+InfinityOfVoid@users.noreply.github.com>
2021-09-16 22:06:44 +0300

    Update lesson1.2.3.c

--- lesson1.2/lesson1.2.3.c
+++ lesson1.2/lesson1.2.3.c
@@ -1,21 +1,5 @@
-void greet() {
-   printf("Hello, ");
-}
-
-void b() {
-   printf("Boris");
-}
-
-void v() {
-   printf("Vladimir");
-}
-
-void print_newline() {
-   printf("\n");
-}
-
-int main() {
-    greet(); b(); print_newline();
-    greet(); v(); print_newline();
-    greet(); b();
-}
+greet(); b();
+print_newline();
+greet(); v();
+print_newline();
+greet(); b();
