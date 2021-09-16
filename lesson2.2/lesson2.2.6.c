e9063870183247891242bff8b5cf8b935d3d41c0
void <90846203+InfinityOfVoid@users.noreply.github.com>
2021-09-16 16:58:53 +0300

    Update lesson2.2.6.c

--- lesson2.2/lesson2.2.6.c
+++ lesson2.2/lesson2.2.6.c
@@ -13,46 +13,41 @@ void marray_print(int64_t** marray, size_t* sizes, size_t rows);
 // если хотя бы одно число NULL, то возвращать второе
 // если оба числа NULL результат NULL
 int64_t* int64_ptr_min(int64_t* x, int64_t* y) {
-  if (x == NULL) {
-      return y;    
-  } else if (y == NULL) {
-      return x;    
-  }
-  if (*x < *y) {
-      return x;    
-  } else {
-      return y;    
-  }
+    if (x == NULL) return y;    
+    else if (y == NULL) return x;    
+    else {
+        if (*x < *y) return x;    
+        else return y; 
+    }
+    return NULL;
 }
 
 // Вернуть адрес минимального элемента массива массивов
 int64_t* marray_int_min( int64_t** marray, size_t* sizes, size_t rows ) {
-  int64_t* marray_min = NULL;
+    int64_t* armin = NULL;
     for (size_t i = 0; i < rows; ++i) {
-        int64_t* row_min = array_int_min(marray[i], sizes[i]);
-        marray_min = int64_ptr_min(marray_min, row_min);
+        int64_t* romin = array_int_min(marray[i], sizes[i]);
+        armin = int64_ptr_min(armin, romin);
     }
-    return marray_min;
+    return armin;
 }
 
 // Вычесть m изо всех элементов массива
 void marray_normalize( int64_t** marray, size_t sizes[], size_t rows, int64_t m ) {
-  for (size_t row = 0; row < rows; ++row) {
-      for (size_t j = 0; j < sizes[row]; ++j) {
-          marray[row][j] -= m;
-      }
-  }
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < sizes[i]; ++j) {
+            marray[i][j] -= m;
+        }
+    }
 }
 
 // Прочитать, найти минимум и нормализовать массив, вывести результат
 void perform() {
-  size_t rows;
-  size_t* sizes;
-  int64_t** marray = marray_read( &rows, &sizes);
-  int64_t* min = marray_int_min(marray, sizes, rows);
-  if (min != NULL)
-    marray_normalize(marray, sizes, rows, *min);
-  marray_print(marray, sizes, rows);
-  marray_free(marray, rows);
-  free(sizes);
+    size_t rows; size_t* sizes;
+    int64_t** marray = marray_read( &rows, &sizes);
+    int64_t* min = marray_int_min(marray, sizes, rows);
+    if (min != NULL) marray_normalize(marray, sizes, rows, *min);
+    marray_print(marray, sizes, rows);
+    marray_free(marray, rows);
+    free(sizes);
 }
