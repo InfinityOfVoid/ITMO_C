881b8490a4c6eeec7e05b082fb4e884e1f6f4a0d
void <90846203+InfinityOfVoid@users.noreply.github.com>
2021-09-16 17:11:42 +0300

    Update lesson2.3.1.c

--- lesson2.3/lesson2.3.1.c
+++ lesson2.3/lesson2.3.1.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+/*
 // Один из двух случаев:
 // - valid = true и value содержит осмысленный результат
 // - valid = false и value может быть любым
@@ -24,31 +25,22 @@ struct maybe_int64 some_int64( int64_t i ) {
 // Все поля инициализированы нулями
 // .value = 0, .valid = false
 const struct maybe_int64 none_int64 = { 0 };
+*/
+
 
 // упакованное число 42 выводится как "Some 42"
 // Ошибка выводится как None
 void maybe_int64_print( struct maybe_int64 i ) {
-    if (i.valid) {
-        printf("Some %" PRId64, i.value);
-    } else {
-        printf("None");
-    }
+    (i.valid == true) ? printf("Some %" PRId64, i.value) : printf("None");
 }
 
 // Если обе упаковки содержат ошибку, то результат ошибка
 // Если ровно одна упаковка содержит ошибку, то результат -- вторая
 // Если обе упаковки содержат число, то результат это минимум из двух чисел.
 struct maybe_int64 maybe_int64_min(struct maybe_int64 a, struct maybe_int64 b) {
-    if (!a.valid) {
-        return b;
-    } else if (!b.valid) {
-        return a;    
-    }
     int64_t min;
-    if (a.value < b.value) {
-        min = a.value;    
-    } else {
-        min = b.value;
-    }
+    if (!a.valid) return b;
+    else if (!b.valid) return a;    
+    min = (a.value < b.value) ? a.value : b.value;
     return (struct maybe_int64) {.valid = true, .value = min};
 }
